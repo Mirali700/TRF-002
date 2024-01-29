cb6dd9ac573d783d1864929ce79d1fefe41b44ab
Mirali700 <miralialiyev2002@gmail.com>
2024-01-29 19:09:02 +0400

    homework

---
+++ prep/homework.dart
@@ -0,0 +1,50 @@
+abstract class Playable {
+  play();
+  pause();
+  stop();
+}
+
+class MediaFile implements Playable {
+  String title;
+  int duration;
+  String filePath;
+
+  MediaFile(this.title, this.duration, this.filePath);
+
+  @override
+  void play() {
+    print('$title started');
+  }
+
+  @override
+  void pause() {
+    print('$title paused');
+  }
+
+  @override
+  void stop() {
+    print('$title stopped');
+  }
+}
+
+mixin Shuffleable {
+  void shufflePlaylist(List<Playable> playlist) {
+    playlist.shuffle();
+    print("Playlist shuffled");
+  }
+}
+
+class Playlist extends MediaFile with Shuffleable {
+  String name;
+  List<Playable> mediaFiles;
+
+  Playlist(this.name, this.mediaFiles) : super("", 0, ""); 
+
+  void playPlaylist() {
+    shufflePlaylist(mediaFiles);
+    for (var mediaFile in mediaFiles) {
+      mediaFile.play();
+    }
+  }
+}
+
